50b1a5fe776213d5e845b8204419f92b2638e021
Margaux Doisy <margauxdoisy@gmail.com>
2024-07-26 15:07:16 +0200

    Initial commit

---
+++ src/main/java/org/example/forum_spring/ForumSpringApplication.java
@@ -0,0 +1,13 @@
+package org.example.forum_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/forum_spring/ForumSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.forum_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
